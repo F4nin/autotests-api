33807ed80d5bf2720aeed4f9dc71f8c5867d7623
F4nin <tovstoles.aleksei@gmail.com>
2026-03-29 19:26:49 +0300

    Практикуемся в написании API автотестов. Часть 2. Практика написания API автотестов для работы с курсами

--- fixtures/authentication.py
+++ fixtures/authentication.py
@@ -1,11 +1,5 @@
 from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 import pytest
-from pydantic import BaseModel, EmailStr
-
-from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
-from clients.users.public_users_client import get_public_users_client, PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
