88532f116c35f752d8b103844d665e917c06d831
F4nin <tovstoles.aleksei@gmail.com>
2026-04-01 23:47:49 +0300

    Настройки автотестов с Pydantic.Практика работы с настройками

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -1,5 +1,6 @@
 from tools.fakers import fake
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, HttpUrl, Field, FilePath
+
 
 class FileSchema(BaseModel):
     """
@@ -16,7 +17,7 @@ class CreateFileRequestSchema(BaseModel):
     """
     filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
     directory: str = Field(default="tests")
-    upload_file: str
+    upload_file: FilePath
 
 class CreateFileResponseSchema(BaseModel):
     """
